9ec51e04564ea6d9cabc2397dc969182ede1e167
Jeong, Heon <blmarket@gmail.com>
2014-05-01 18:06:52 +0900

    Label for various classes.

--- labeler.py
+++ labeler.py
@@ -5,11 +5,7 @@ def label(row):
     img = np.reshape(row, (52, 1024))
     io.imshow(img)
 
-    yn = raw_input('YN? : ')
-    if yn == 'Y' or yn == 'y':
-        return 1
-    else:
-        return 0
+    return raw_input('label? : ')
 
 ## Current trials: df.loc[3, 'label'] = label(df.loc[3])
 ## type is problem.
