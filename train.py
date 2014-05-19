c72a0ae558a57062ae6e005a5cfd6dc4e705310b
Jeong, Heon <heon.jeong@sk.com>
2014-05-19 17:21:38 +0900

    Total rewrite of train.py

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import pandas as pd
 import MySQLdb as mdb
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier as RF
+from skimage import io
 import numpy as np
 import json
 
@@ -14,9 +15,55 @@ config = json.load(open('config.json'))
 
 con = mdb.connect(config[u'host'], config[u'user'], config[u'password'], config[u'database'])
 
-cur = con.cursor()
+def getOne(pid):
+    cur = con.cursor()
+    cur.execute("SELECT id, type, field_name FROM `predictions` WHERE `id` = '%s' LIMIT 1" % (pid))
+    row = cur.fetchone()
+    cur.close()
+    return row
 
-cur.execute("SELECT * FROM `predictions` WHERE `id` = '%s' LIMIT 1" % (1))
-row = cur.fetchone()
+def getTrain(info):
+    label = info[2]
+    df = pd.io.sql.read_frame(
+        '''
+        SELECT `src_id`, DATA, `%s` 
+        FROM `src` LEFT JOIN `samples` ON `src`.`id` = `samples`.`src_id`
+        WHERE `%s` IS NOT NULL ORDER BY RAND() LIMIT 100
+        ''' % (label, label), con
+    )
+    X = pd.DataFrame(list(df['DATA'].map(lambda x: np.array(pickle.loads(x), dtype=np.float64))))
+    y = df[label]
 
-print row
+    rf = RF()
+    rf.fit(X, y)
+    return rf
+
+def getTest(model, info):
+    label = info[2]
+    df = pd.io.sql.read_frame(
+        '''
+        SELECT `src`.`id` AS `src_id`, DATA, `%s` 
+        FROM `src` LEFT JOIN `samples` ON `src`.`id` = `samples`.`src_id`
+        WHERE src.type = 1 AND `%s` IS NULL LIMIT 1000
+        ''' % (label, label), con
+    )
+
+    X = pd.DataFrame(list(df['DATA'].map(lambda x: np.array(pickle.loads(x), dtype=np.float64))))
+    result = model.predict(X)
+    df['predict_result'] = result
+    df['pid'] = info[0]
+    return df.drop(['DATA', label], axis=1)
+
+def putResult(df):
+    print df
+    pd.io.sql.write_frame(df, 'predict_result', con, flavor = 'mysql', if_exists='append')
+
+info = getOne(1)
+model = getTrain(info)
+putResult(getTest(model, info))
+
+# print getTrain(getOne(1))
+# getOne(2)
+# getOne(3)
+# getOne(4)
+# getOne(5)
