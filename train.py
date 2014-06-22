d5cdc02df6f83e7076c8f50b654568662c3f7e6e
Jeong, Heon <heon.jeong@sk.com>
2014-06-23 02:05:28 +0900

    Change DB behavior.
    use sqlalchemy engine interface instead of old DBAPI.

--- train.py
+++ train.py
@@ -4,7 +4,9 @@
 Train and create prediction.
 FIXME: duplicate code with train.py
 """
-import sys
+from sys import exit
+from StringIO import StringIO
+import sqlalchemy as sa
 import pickle
 import pandas as pd
 import MySQLdb as mdb
@@ -14,21 +16,29 @@ from skimage import io
 import numpy as np
 import json
 
-config = json.load(open('config.json'))
+config = None
+with open('config.json') as conf:
+    config = json.load(conf)
 
-con = mdb.connect(config[u'host'], config[u'user'], config[u'password'], config[u'database'], use_unicode = True, charset = 'utf8')
+if not config:
+    exit(-1)
+
+conn_str = "mysql://%s:%s@%s/%s" % (config[u'user'], config[u'password'], config['host'], config['database'])
+
+engine = sa.create_engine(conn_str, encoding = 'utf8')
 
 def getTrain(label):
-    df = pd.io.sql.read_frame(
+    df = pd.io.sql.read_sql(
         '''
         SELECT `src_id`, `DATA`, `value` 
         FROM `src` LEFT JOIN `tags` ON `src`.`id` = `tags`.`src_id`
         WHERE `name` = '%s' AND `probability` IS NULL
-        ''' % (label), con
+        ''' % (label), engine
     )
     print df
     print "REad data complete"
-    X = pd.DataFrame(list(df['DATA'].map(lambda x: np.array(pickle.loads(x), dtype=np.float64))))
+
+    X = pd.DataFrame(list(df['DATA'].map(lambda x: np.load(StringIO(x)))))
     y = df['value']
 
     rf = RF(n_jobs = 3)
@@ -37,16 +47,21 @@ def getTrain(label):
     return rf
 
 def getPrediction(model, label):
-    df = pd.io.sql.read_frame(
+    df = pd.io.sql.read_sql(
         '''
         SELECT `id` AS `src_id`, DATA FROM `src`
         WHERE `type` = '1' AND `id` NOT IN (
             SELECT src_id FROM tags WHERE `name` = '%s'
         ) LIMIT 1000
-        ''' % (label), con
+        ''' % (label), engine
     )
+    
+    print len(df)
 
-    X = pd.DataFrame(list(df['DATA'].map(lambda x: np.array(pickle.loads(x), dtype=np.float64))))
+    if len(df) == 0:
+        return None
+
+    X = pd.DataFrame(list(df['DATA'].map(lambda x: np.load(StringIO(x)))))
     result = model.predict(X)
 
     df['name'] = label
@@ -55,10 +70,12 @@ def getPrediction(model, label):
     return df.drop(['DATA'], axis=1)
 
 def putResult(df):
-    pd.io.sql.write_frame(df, 'tags', con, flavor = 'mysql', if_exists='append')
+    pd.io.sql.write_frame(df, 'tags', engine, flavor = 'mysql', if_exists='append')
 
 for label_name in [ 'clan_place', 'name', 'attack1', 'attack2', 'total_stars' ]:
     model = getTrain(label_name)
     prediction = getPrediction(model, label_name)
+    if prediction is None:
+        continue
     print prediction
     putResult(prediction)
