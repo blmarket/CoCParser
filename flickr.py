559985a70a65dd562d12e53c9d2f7321602c2970
Jeong, Heon <blmarket@gmail.com>
2014-11-09 11:09:56 +0900

    FlickrAPI updated to 2.0

--- flickr.py
+++ flickr.py
@@ -1,5 +1,6 @@
 import flickrapi
 from urllib import urlretrieve
+import json
 
 # TARGET_TITLE = '20140501'
 
@@ -8,17 +9,22 @@ from urllib import urlretrieve
 # api key is public information or not, anyway, there is no
 # problem of exposing my api key.)
 api_key = 'c165ac58362fc00ad075c667ef060dc1'
+api_secret = '7f5c05b7d82779ba'
 myid = '123519224@N08'
 
 def fetch_images(title):
-    fl = flickrapi.FlickrAPI(api_key)
-    res = fl.photos_search(user_id = myid, text=title, per_page = '20', format = 'etree', extras = 'url_o')
+    fl = flickrapi.FlickrAPI(api_key, api_secret)
+    res = fl.photos_search(
+            user_id = myid, per_page = '20',
+            extras = 'url_o',
+            format = 'json')
 
-    photos = res.find('photos').findall('photo')
+    photos = json.loads(res)[u'photos']['photo']
 
-    for i in range(len(photos)):
-        photo = photos[i]
-        url = dict(photo.items())['url_o']
+    for photo in photos:
+        if photo[u'title'] != title:
+            continue
+        url = photo[u'url_o']
         yield url
 
 def download_images(title):
@@ -29,7 +35,7 @@ def download_images(title):
 
 if __name__ == "__main__":
     # 1. download them
-    download_images('20140617')
+    download_images('20141109')
     # 2. list images
     # for it in fetch_images('20140501'):
     #     print it
