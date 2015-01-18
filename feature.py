a762f3638093389b448844ca8acc424f8d559829
Heon Jeong <blmarket@linecorp.com>
2015-01-18 15:26:51 +0900

    Removed google drive integration.
    we're using own hosting.

--- feature.py
+++ feature.py
@@ -5,20 +5,14 @@ TODO: fill this area
 """
 from sys import exit
 from io import BytesIO
-import sqlalchemy as sa
-import pickle
-import pandas as pd
-from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier as RF
 from skimage import io
 from skimage import feature
 import numpy as np
 import json
-import engine
 import db_mysql
 from matplotlib import pyplot as plt
 
-for it in range(4031, 4041):
+for it in range(4031, 4033):
     x = np.load(BytesIO(db_mysql.cache_mysql(str(it)))).reshape([52, 1024])
 
     detector = feature.CENSURE()
