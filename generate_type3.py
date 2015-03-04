795cbea6a327c9abc145ee253ba18eca9e242cdc
Heon Jeong <blmarket@linecorp.com>
2015-03-05 00:31:58 +0900

    Marked todo

--- generate_type3.py
+++ generate_type3.py
@@ -8,6 +8,7 @@ from models.models import War, Src
 from sqlalchemy.orm import Session
 from slit_utils import cutfront
 
+import numpy as np
 import blobs
 from StringIO import StringIO
 from skimage import io
@@ -19,14 +20,14 @@ session = Session(engine)
 def add_splits(date, src_id):
     def save_img(arr):
         """saves image into src"""
-        # png = StringIO()
-        # io.imsave(png, arr)
-        # png_url = blobs.createBlob(date, png.getvalue())
+        png = StringIO()
+        io.imsave(png, arr)
+        png_url = blobs.createBlob(date, png.getvalue())
 
         data_fp = StringIO()
         np.save(data_fp, arr)
 
-        print data_fp
+        # TODO: Save image and data into src table and return its id
 
     save_img(cutfront((src_id, 0)))
     save_img(cutfront((src_id, 1)))
